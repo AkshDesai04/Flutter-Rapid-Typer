ae89d7121c4cd77277602b4353e5d30681653614
AkshDesai04 <dev.akshdesai@gmail.com>
2022-10-23 14:22:07 +0530

    Added ket detection and removed test folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,90 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const RapidTyper());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class RapidTyper extends StatelessWidget {
+  const RapidTyper({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        body: const MyKeyExample(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MyKeyExample extends StatefulWidget {
+  const MyKeyExample({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyKeyExample> createState() => _MyKeyExampleState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyKeyExampleState extends State<MyKeyExample> {
+  // The node used to request the keyboard focus.
+  final FocusNode _focusNode = FocusNode();
+  // The message to display.
+  String? _message = "";
 
-  void _incrementCounter() {
+  // Focus nodes need to be disposed.
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  // Handles the key events from the Focus widget and updates the
+  // _message.
+  KeyEventResult _handleKeyEvent(FocusNode node, RawKeyEvent event) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      if (event.logicalKey == LogicalKeyboardKey.keyQ) {
+        // _message = 'Pressed the "Q" key!';
+      } else {
+        if (kReleaseMode) {
+          // _message ='Not a Q: Pressed 0x${event.logicalKey.keyId.toRadixString(16)}';
+        } else {
+          // As the name implies, the debugName will only print useful
+          // information in debug mode.
+          _message = '$_message${event.logicalKey.debugName}';
+        }
+      }
     });
+    return event.logicalKey == LogicalKeyboardKey.keyQ
+        ? KeyEventResult.handled
+        : KeyEventResult.ignored;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    final TextTheme textTheme = Theme.of(context).textTheme;
+    return Container(
+      color: Colors.white,
+      alignment: Alignment.center,
+      child: DefaultTextStyle(
+        style: textTheme.headline4!,
+        child: Focus(
+          focusNode: _focusNode,
+          onKey: _handleKeyEvent,
+          child: AnimatedBuilder(
+            animation: _focusNode,
+            builder: (BuildContext context, Widget? child) {
+              if (!_focusNode.hasFocus) {
+                return GestureDetector(
+                  onTap: () {
+                    FocusScope.of(context).requestFocus(_focusNode);
+                  },
+                  child: const Text('Click to focus'),
+                );
+              }
+              return Text(_message ?? 'Press a key');
+            },
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
