afd4ecfab182a0fc09a359fba30bb6213d8e279a
AkshDesai04 <dev.akshdesai@gmail.com>
2022-10-26 19:56:34 +0530

    Added more fail safes and added static text up above

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'dart:math';
 
 void main() => runApp(const RapidTyper());
 
@@ -28,7 +29,10 @@ class _MyKeyExampleState extends State<MyKeyExample> {
   // The node used to request the keyboard focus.
   final FocusNode _focusNode = FocusNode();
   // The message to display.
-  String? _message = "";
+  String _message = "", temp = "";
+  var char_history = [];
+  int IDO = 0;
+  final int mi = -1;
 
   // Focus nodes need to be disposed.
   @override
@@ -38,27 +42,49 @@ class _MyKeyExampleState extends State<MyKeyExample> {
   }
 
   String getChar(String data) {
-    if (data.indexOf("Key ") == 0) data = data[4];
-    if (data.indexOf("Digit ") == 0) data = data[6];
-    print(data);
-    return data;
+    print("Raw data: $data");
+
+    data = data.toLowerCase();
+
+    if (data.indexOf("key ") == 0) data = data[4];
+    if (data.indexOf("digit ") == 0) data = data[6];
+    if (data.indexOf("numpad ") == 0) data = data[7];
+    if (data == 'space') data = ' ';
+    if (data == 'comma') data = ',';
+    if (data == 'period') data = '.';
+    if (data == 'slash') data = '/';
+    if (data == 'backslash') data = '\\';
+    if (data.indexOf("al") == 0) data = ""; //alt
+    if (data.indexOf("sh") == 0) data = ""; //shift
+    if (data.indexOf("co") == 0) data = ""; //control
+    if (data.indexOf("me") == 0) data = ""; //meta
+    if (data.indexOf("en") == 0) data = "\n"; //enter
+    if (data.indexOf("ta") == 0) data = ""; //tab
+
+    IDO = char_history.indexOf(data);
+    print("outside index = $IDO");
+
+    if (IDO < 0) {
+      char_history.add(data);
+      if (data == "backspace") {
+        //backspace
+        _message = _message.substring(0, _message.length - 1);
+        return "";
+      }
+      return data;
+    }
+    char_history.remove(data);
+    if (data == "backspace") return "";
+    return "";
   }
 
-  // Handles the key events from the Focus widget and updates the
-  // _message.
   KeyEventResult _handleKeyEvent(FocusNode node, RawKeyEvent event) {
     setState(() {
-      if (event.logicalKey == LogicalKeyboardKey.keyQ) {
-        // _message = 'Pressed the "Q" key!';
-      } else {
-        if (kReleaseMode) {
-          // _message ='Not a Q: Pressed 0x${event.logicalKey.keyId.toRadixString(16)}';
-        } else {
-          // As the name implies, the debugName will only print useful
-          // information in debug mode.
-          // _message = '$_message${event.logicalKey.debugName}';
-          _message = getChar('${event.logicalKey.debugName}');
-        }
+      if (!kReleaseMode) {
+        // As the name implies, the debugName will only print useful
+        // information in debug mode.
+        temp = getChar('${event.logicalKey.debugName}');
+        _message = '$_message' + temp;
       }
     });
     return event.logicalKey == LogicalKeyboardKey.keyQ
@@ -69,27 +95,55 @@ class _MyKeyExampleState extends State<MyKeyExample> {
   @override
   Widget build(BuildContext context) {
     final TextTheme textTheme = Theme.of(context).textTheme;
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    double min_hw = min(height, width);
     return Container(
       color: Colors.white,
       alignment: Alignment.center,
-      child: DefaultTextStyle(
-        style: textTheme.headline4!,
-        child: Focus(
-          focusNode: _focusNode,
-          onKey: _handleKeyEvent,
-          child: AnimatedBuilder(
-            animation: _focusNode,
-            builder: (BuildContext context, Widget? child) {
-              if (!_focusNode.hasFocus) {
-                return GestureDetector(
-                  onTap: () {
-                    FocusScope.of(context).requestFocus(_focusNode);
-                  },
-                  child: const Text('Click to focus'),
+      child: Padding(
+        padding: EdgeInsets.all(min_hw * 0.25),
+        child: DefaultTextStyle(
+          style: textTheme.headline4!,
+          child: Focus(
+            focusNode: _focusNode,
+            onKey: _handleKeyEvent,
+            child: AnimatedBuilder(
+              animation: _focusNode,
+              builder: (BuildContext context, Widget? child) {
+                if (!_focusNode.hasFocus) {
+                  return GestureDetector(
+                    onTap: () {
+                      FocusScope.of(context).requestFocus(_focusNode);
+                    },
+                    child: const Text(
+                      "Let's Get Started\nClick Here",
+                      textAlign: TextAlign.center,
+                    ),
+                  );
+                }
+                return Column(
+                  children: [
+                    Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black),
+                        ),
+                        child: Text("hello world this is aksh desai")),
+                    Text(
+                      _message ?? 'Press a key',
+                      style: TextStyle(
+                        color: Colors.red,
+                      ),
+                    ),
+                  ],
                 );
-              }
-              return Text(_message ?? 'Press a key');
-            },
+                // return RichText(
+                //   text: TextSpan(
+                //     children: textspans,
+                //   ),
+                // );
+              },
+            ),
           ),
         ),
       ),
